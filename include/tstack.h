1053c11e232363a5c14799ce33d79c7365fe4294
MrTouchpad <144840383+MrTouchpad@users.noreply.github.com>
2026-04-18 15:57:54 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,40 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T data[size];
+  int top_index;
+
+ public:
+  TStack() : top_index(0) {}
+
+  void push(const T& value) {
+    if (top_index < size) {
+      data[top_index++] = value;
+    }
+  }
+
+  T pop() {
+    if (top_index > 0) {
+      return data[--top_index];
+    }
+    return T();
+  }
+
+  T top() const {
+    if (top_index > 0) {
+      return data[top_index - 1];
+    }
+    return T();
+  }
+
+  bool is_empty() const {
+    return top_index == 0;
+  }
+
+  int size() const {
+    return top_index;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
